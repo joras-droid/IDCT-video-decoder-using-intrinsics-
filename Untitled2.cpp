e70da61ce1715ba6eef957e37f966932a43ffc53
droid <65585311+joras-droid@users.noreply.github.com>
2025-11-09 14:11:56 +0545

    one rgb macroblock manual creation

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -2,24 +2,31 @@
 #include <iostream>
 
 int main() {
-    int width = 1920, height = 1080;
-    cv::Mat img(height, width, CV_8UC3);
+    int block_size = 8;
+
+    int width = 920, height = 780; 
+    cv::Mat block(block_size, block_size, CV_8UC3);
+    //cv::Mat img(height, width, CV_8UC3);
 
     // fill pixels with a color pattern (demo)
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
+    for (int y = 0; y < block_size; ++y) {
+        for (int x = 0; x < block_size; ++x) {
             //RGB order for OPencv
-            img.at<cv::Vec3b>(y, x)[0] = x % 256;
-            img.at<cv::Vec3b>(y, x)[1] = y % 256;
-            img.at<cv::Vec3b>(y, x)[2] = (x+y) % 256;
-
+            if (y % 2 == 0) {
+                block.at<cv::Vec3b>(y, x) = cv::Vec3b(255, 0, 0);
+            }
+            else {
+                block.at<cv::Vec3b>(y, x) = cv::Vec3b(0, 0, 255);
+            }
         }
     }
+    cv::Mat block_big;
+    cv::resize(block, block_big, cv::Size( 320,  320), 0, 0, cv::INTER_NEAREST);
         //cv::namedWindow("Colorful Pixels", cv::WINDOW_NORMAL);
         //cv::setWindowProperty("Colorful Pixels", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
 
         cv::imshow(
-            "colorful pixels", img
+            "one macroblock", block_big
         );
         cv::waitKey(0);
         return 0;
